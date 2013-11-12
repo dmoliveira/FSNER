292884ac7c1c1a80f1dbd9ea8e72ce74d3c3810b
Diego Oliveira <diego@zunnit.com>
2013-11-12 10:48:56 -0200

    Adjusting Label Level 2.

--- src/lbd/data/handler/SequenceSet.java
+++ src/lbd/data/handler/SequenceSet.java
@@ -1,5 +1,6 @@
 package lbd.data.handler;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +39,32 @@ public class SequenceSet implements IDataIterator {
 
 	public int size() {
 		return (mSequenceList.size());
+	}
+
+	public static void checkSequenceSetAreSameSize(SequenceSet pSequenceSetA, SequenceSet pSequenceSetB) {
+		if(pSequenceSetA.size() != pSequenceSetB.size()) {
+			throw new ArrayIndexOutOfBoundsException(MessageFormat.format("Error: Size of SequenceSet A and B are different. A: {0} and B:{1}.",
+					pSequenceSetA.size(), pSequenceSetB.size()));
+		}
+
+		while(pSequenceSetA.hasNext()) {
+			checkSequenceAreSameTokens(pSequenceSetA.next(), pSequenceSetB.next());
+
+		}
+	}
+
+	public static void checkSequenceAreSameTokens(ISequence pSequenceA, ISequence pSequenceB) {
+		if(pSequenceA.length() != pSequenceB.length()) {
+			throw new ArrayIndexOutOfBoundsException(MessageFormat.format("Error: Length of Sequence A and B are different. A: {0} and B:{1}.",
+					pSequenceA.length(), pSequenceB.length()));
+		}
+
+		for(int i = 0; i < pSequenceA.length(); i++) {
+			if(!pSequenceA.getToken(i).equals(pSequenceB.getToken(i))) {
+				throw new ArrayIndexOutOfBoundsException(MessageFormat.format("Error: Token of Sequence A and B are different. A: {0} and B:{1}.",
+						pSequenceA.getToken(i), pSequenceB.getToken(i)));
+			}
+		}
 	}
 
 }
